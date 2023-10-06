ff009593bb48fc9851ea1614d3c8dac344ebc25d
Jaxcoder <codenamejason@outlook.com>
2023-10-06 16:39:43 -0400

    init: new foundry project

---
+++ test/Will4USNFT.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Will4USNFT} from "../src/Will4USNFT.sol";
+
+contract Will4USNFTTest is Test {
+    Will4USNFT public nftContract;
+
+    function setUp() public {
+        nftContract = new Will4USNFT();
+    }
+
+    function test_Mint() public {}
+
+    function test_UpdateMetadata() public {}
+}
